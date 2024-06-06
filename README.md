2ec23011efa20c2fb04af3558833bd49cf705788
Suraj Pandey <suraj.pandey@affinity.com>
2024-06-06 13:23:33 +0530

    node api code

---
+++ README.md
@@ -0,0 +1,3 @@
+# REST_API_WITH_MYSQL
+- 👋 Hi, I’m @Tariqu
+- 👀 I’m looking for contributor
---
+++ api/users/user.controller.js
@@ -0,0 +1,135 @@
+const {
+  create,
+  getUserByUserEmail,
+  getUserByUserId,
+  getUsers,
+  updateUser,
+  deleteUser
+} = require("./user.service");
+const { hashSync, genSaltSync, compareSync } = require("bcrypt");
+const { sign } = require("jsonwebtoken");
+
+module.exports = {
+  createUser: (req, res) => {
+    const body = req.body;
+    const salt = genSaltSync(10);
+    body.password = hashSync(body.password, salt);
+    create(body, (err, results) => {
+      if (err) {
+        console.log(err);
+        return res.status(500).json({
+          success: 0,
+          message: "Database connection errror"
+        });
+      }
+      return res.status(200).json({
+        success: 1,
+        data: results
+      });
+    });
+  },
+  login: (req, res) => {
+    const body = req.body;
+    getUserByUserEmail(body.email, (err, results) => {
+      if (err) {
+        console.log(err);
+      }
+      console.log('Database response:', results);
+
+      if (!results ) {
+        return res.json({
+          success: 0,
+          data: "Invalid email or password"
+        });
+      }
+      console.log('body.password, results.password:', body.password, results.password);
+      const result = body.password === results.password;
+      console.log('body.password,result:', result);
+
+      if (body.password === results.password) {
+        results.password = undefined;
+        const jsontoken = sign({ result: results }, "qwe1234", {
+          expiresIn: "1h"
+        });
+        return res.json({
+          success: 1,
+          message: "login successfully",
+          token: jsontoken
+        });
+      } else {
+        return res.json({
+          success: 0,
+          data: "Invalid email or password"
+        });
+      }
+    });
+  },
+  getUserByUserId: (req, res) => {
+    const id = req.params.id;
+    getUserByUserId(id, (err, results) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      if (!results) {
+        return res.json({
+          success: 0,
+          message: "Record not Found"
+        });
+      }
+      results.password = undefined;
+      return res.json({
+        success: 1,
+        data: results
+      });
+    });
+  },
+  getUsers: (req, res) => {
+    getUsers((err, results) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      return res.json({
+        success: 1,
+        data: results
+      });
+    });
+  },
+  updateUsers: (req, res) => {
+    const body = req.body;
+    const salt = genSaltSync(10);
+    body.password = hashSync(body.password, salt);
+    updateUser(body, (err, results) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      return res.json({
+        success: 1,
+        message: "updated successfully"
+      });
+    });
+  },
+  deleteUser: (req, res) => {
+    const data = req.body;
+    deleteUser(data, (err, results) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      console.log('Database response:', results.affectedRows);
+
+      if (results.affectedRows == 0) {
+        return res.json({
+          success: 1,
+          message: "Record Not Found"
+        });
+      }
+      return res.json({
+        success: 0,
+        message: "user deleted successfully"
+      });
+    });
+  }
+};
